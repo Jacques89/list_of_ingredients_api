245d3b27546fa4a58cf8e6d17edd350b4627fcdd
Jacques Nalletamby <jacknalletamby@gmail.com>
2019-04-08 19:42:27 +0200

    initial commit

---
+++ db/migrate/20190408151813_create_drinks.rb
@@ -0,0 +1,13 @@
+class CreateDrinks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :drinks do |t|
+      t.string :title
+      t.string :description
+      t.string :steps
+      t.string :string
+      t.string :source
+
+      t.timestamps
+    end
+  end
+end
